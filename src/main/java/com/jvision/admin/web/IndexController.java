4990d4fe9a585870da96f508638a23b60ccd2a27
unknown <mg02070@hub.com>
2020-11-30 15:29:04 +0900

    springboot-webservice

---
+++ src/main/java/com/jvision/admin/web/IndexController.java
@@ -0,0 +1,33 @@
+package com.jvision.admin.web;
+
+import com.jvision.admin.service.PostsService;
+import com.jvision.admin.web.dto.PostsResponseDto;
+import com.jvision.admin.web.dto.PostsSaveRequestDto;
+import com.jvision.admin.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave()
+    {
+        return "posts-save";
+    }
+
+
+
+}
